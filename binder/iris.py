a34e01644b4ca99833f88d169a2ca085b0d4852e
draves15 <87029202+draves15@users.noreply.github.com>
2021-07-26 23:38:03 -0400

    Update iris.py

--- binder/iris.py
+++ binder/iris.py
@@ -3,6 +3,28 @@
 # 1.1 Install SciPy Libraries
 # see enviroment.yml
 
+# 1.2 Start Binder and Create a Jupyter Notebook to check Python and Versions
+# Check the versions of libraries
+ 
+# Python version
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+
 # Load libraries
 from pandas import read_csv
 from pandas.plotting import scatter_matrix
